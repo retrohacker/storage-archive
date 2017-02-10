239c172382bd8105aa99ea51d96da2f4c3b1b0b4
retrohacker <william.jblankenship@gmail.com>
2017-02-10 15:59:11 -0500

    Let there be files

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 Collection of papers on untrusted distributed storage networks.
 
 Copyright for each paper belongs to the original publisher.
+
+### Building
+
+This generates a static website that serves the PDFs in `files/` directly from the storj network.
+
+To upload the files to storj, simply run:
+
+```js
+storj add-bucket storage-archive
+// creates bucket 83b67934372902f0422c0ef7
+storj generate-seed
+storj make-public --pull 83b67934372902f0422c0ef7
+STORJ_EMAIL=[username] STORJ_PASSWORD=[pass] npm run build
+```
+
+You can also supply `STORJ_BRIDGE=[http://bridge_ip:port]` if you want to use another bridge for the upload.
+
+From here, jekyll takes over, all you need to do is upload to GitHub pages.
+
+If you want to test it locally, simply run:
+
+```
+npm install -g local-web-server
+jekyll build
+cd _site
+ws -p 8000
+```
+
+And the site will now be served from http://127.0.0.1:8000
--- upload.js
+++ upload.js
@@ -1,6 +1,9 @@
 const async = require('async');
 const glob = require('glob');
 const Storj = require('storj');
+const path = require('path');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 const result = {}
 const bucketName = process.env.BUCKET_NAME || 'storage-archive';
@@ -29,35 +32,50 @@ function validateInput() {
   if(opts.basicAuth.email === undefined) {
     return handleError(new Error('STORJ_EMAIL required'));
   }
+
+  if(bucketName === undefined) {
+    return handleError(new Error('BUCKET_NAME required'));
+  }
 }
 
 /**************
  * Main Logic *
  **************/
 
-let storj = new Storj();
+validateInput();
+
+let storj = new Storj(opts);
 storj.on('ready', function(e) {
   if(e) {
     return handleError(e);
   }
-  var keypair = storj.generateKeyPair();
-  var pubkey = keypair.getPublicKey();
-  storj.registerKey(pubkey, function(e) {
-    console.log(arguments);
-  });
+  // Start waterfall below
+  getBucketId();
 });
 
 /*********************
  * Hoist async logic *
  *********************/
 
-function createBucket() {
+function getBucketId() {
   // Only create a bucket if one doesn't already exist
   storj.getBuckets(function(e, buckets) {
     if(e) {
       return handleError(e);
     }
-    console.log(buckets);
+
+    for(var i = 0; i < buckets.length; i++) {
+      if(buckets[i].name === bucketName) {
+        result.bucketId = buckets[i].id;
+        break;
+      }
+    }
+
+    if(result.bucketId === undefined) {
+      return handleError('Did not find bucket');
+    }
+
+    getFiles()
   });
 }
 
@@ -66,6 +84,69 @@ function getFiles() {
     if(e) {
       return handleError(e);
     }
+
+    result.files = files.map((v) => ({
+      path: v,
+      name: path.parse(v).base
+    }));
+
+    getBucketContents();
   })
 }
 
+function getBucketContents() {
+  storj.getBucket(result.bucketId, function(e, bucket) {
+    if(e) {
+      return handleError(e);
+    }
+
+    for(var i = 0; i < bucket.files.length; i++) {
+      for(var j = 0; j < result.files.length; j++) {
+        if(bucket.files[i].filename === result.files[j].name) {
+          result.files[j].id = bucket.files[i].id;
+        }
+      }
+    }
+    uploadFiles();
+  });
+}
+
+function uploadFiles() {
+  async.eachLimit(result.files, 5, uploadFile, function(e) {
+    if(e) {
+      return handleError(e);
+    }
+
+    console.log('Saving info to bucket.json....');
+    mkdirp('./_data', function(e) {
+      if(e) {
+        return handleError(e);
+      }
+      fs.writeFileSync('./_data/bucket.json', JSON.stringify(result));
+    });
+    console.log('DONE!');
+  });
+}
+
+function uploadFile(file, cb) {
+  if(file.id !== undefined) {
+    console.log(`${file.name} already in bucket, skipping!`);
+    return cb();
+  }
+
+  var rs = fs.createReadStream(file.path);
+
+  console.log(`${file.name}: Uploading...`);
+  var f = storj.createFile(result.bucketId, file.name, rs);
+  f.on('error', function(e) {
+    var c = cb;
+    cb = function noop() {}
+    console.error(`${file.name} Failed to upload`);
+    console.error(e);
+    return c();
+  });
+  f.on('done', function(resp) {
+    file.id = resp.id;
+    return cb();
+  });
+}
